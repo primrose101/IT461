0475a9615a8edadd49914aacff7a1ce0794ee6e5
primrose101 <purityrosemanlosa@gmail.com>
2022-05-18 21:42:59 +0800

    Seatwork #6 - JWT users

--- server/src/app.py
+++ server/src/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, jsonify, g
 from v1.dog.router import DogRouter
 from v1.cat.router import CatRouter
-
 from v1.auth import login as auth_login, verify_token as auth_verify_token
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'I/L0ve/CIT-U'
 
 app.register_blueprint(DogRouter.handler())
+app.register_blueprint(CatRouter.handler())
+
 
 @app.route('/v1/login', methods=['POST'])
 def login():
@@ -25,9 +26,6 @@ def verify_token():
         return jsonify({'message': 'Invalid token'}), 403
     return jsonify({'ok': 'Token is valid'})
 
-bp_cats = Blueprint('cats', __name__, url_prefix='/v1/cats')
-CatRouter.handler(bp_cats)
-app.register_blueprint(bp_cats)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=6000)
--- server/src/v1/auth.py
+++ server/src/v1/auth.py
@@ -18,7 +18,7 @@ def verify_token(token):
 
 def login(username, password):
     # TODO: use the database to verify the username and password
-    if username == 'admin' and password == 'admin':
+    if username == 'prim' and password == 'admin123':
         payload = {
             'username': username,
             'id': 100,
